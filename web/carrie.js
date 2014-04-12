decd782f6380fd819185dd37f1c40b72d9d23128
José Pedro Dias <jose.pedro.dias@gmail.com>
2014-04-12 01:11:25 +0100

    fixed resize, indentation 2 spaces

--- web/carrie.js
+++ web/carrie.js
@@ -1,171 +1,182 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	var sum = function(prev, v) { return prev + v; };
+  var sum = function(prev, v) { return prev + v; };
 
-	var r = function(v) { return Math.round(v); };
+  var r = function(v) { return Math.round(v); };
 
-	var carrie = function(o) {
-		var kbProfile = o.kbProfile;
-		var kbStyle   = o.kbStyle;
-		var socket    = o.socket;
-		var keyMap    = o.keyMap;
+  var carrie = function(o) {
+    var kbProfile = o.kbProfile;
+    var kbStyle   = o.kbStyle;
+    var socket    = o.socket;
+    var keyMap    = o.keyMap;
 
-		//kbProfile, kbStyle) {
-		// for now we're assuming keyboard to take over the whole viewport
+    var W, H, I, cells, canvasEl, ctx;
 
-		// keyboard dimensions in pixels
-		var W = window.innerWidth;
-		var H = window.innerHeight;
+    var reinit = function() {
+      var firstCall = !canvasEl;
 
-		// if keyboard too tall, keep it 3/1 scale
-		if (W/H < 3) { H = ~~(W / 3); }
+      W = window.innerWidth;
+      H = window.innerHeight;
 
-		// create canvas and attach it to document body
-		var canvasEl = document.createElement('canvas');
-		canvasEl.setAttribute('width',  W);
-		canvasEl.setAttribute('height', H);
-		document.body.appendChild(canvasEl);
+      // if keyboard too tall, keep it 3/1 scale
+      if (W/H < 3) { H = ~~(W / 3); }
 
-		var ctx = canvasEl.getContext('2d');
+      if (firstCall) {
+        canvasEl = document.createElement('canvas');  
+      }
 
-		ctx.fillStyle = kbStyle.backgroundColor;
-		ctx.fillRect(0, 0, W, H);
+      canvasEl.setAttribute('width',  W);
+      canvasEl.setAttribute('height', H);
+      
+      if (firstCall) {
+        document.body.appendChild(canvasEl);  
+      }
 
-		ctx.lineWidth = 1;
-		ctx.textAlign = 'center';
-		ctx.textBaseline = 'middle';
+      ctx = canvasEl.getContext('2d');
+      ctx.fillStyle = kbStyle.backgroundColor;
+      ctx.fillRect(0, 0, W, H);
 
-		var I, cells = [];
+      ctx.lineWidth = 1;
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+    };    
 
-		var draw = function(c, isDown) {
-			ctx.fillStyle = isDown ? kbStyle.keyDownBackgroundColor : kbStyle.keyBackgroundColor;
-			ctx.fillRect(c.x0, c.y0, c.w, c.h);
-			if (!isDown) {
-				ctx.strokeRect(c.x0, c.y0, c.w, c.h);
-			}
-			ctx.fillStyle = isDown ? kbStyle.keyDownLabelColor : kbStyle.keyLabelColor;
+    var draw = function(c, isDown) {
+      ctx.fillStyle = isDown ? kbStyle.keyDownBackgroundColor : kbStyle.keyBackgroundColor;
+      ctx.fillRect(c.x0, c.y0, c.w, c.h);
+      if (!isDown) {
+        ctx.strokeRect(c.x0, c.y0, c.w, c.h);
+      }
+      ctx.fillStyle = isDown ? kbStyle.keyDownLabelColor : kbStyle.keyLabelColor;
 
-			var l0 = c.l[0];
-			var l1 = c.l[1];
-			var l2 = c.l[2];
+      var l0 = c.l[0];
+      var l1 = c.l[1];
+      var l2 = c.l[2];
 
-			if (l1 === undefined) {
-				ctx.fillText(c.l, c.w/2+c.x0, c.h/2+c.y0);
-			}
-			else {
-				ctx.fillText(l0, c.w/2+c.x0, c.h*3/4+c.y0);
+      if (l1 === undefined) {
+        ctx.fillText(c.l, c.w/2+c.x0, c.h/2+c.y0);
+      }
+      else {
+        ctx.fillText(l0, c.w/2+c.x0, c.h*3/4+c.y0);
 
-				if (l1 !== undefined) {
-					ctx.fillText(l1, c.w/2+c.x0, c.h/4+c.y0);
-				}
-				
-				if (l2 !== undefined) {
-					ctx.fillText(l2, c.w*3.5/4+c.x0, c.h/4+c.y0);
-				}
-			}
-		};
+        if (l1 !== undefined) {
+          ctx.fillText(l1, c.w/2+c.x0, c.h/4+c.y0);
+        }
+        
+        if (l2 !== undefined) {
+          ctx.fillText(l2, c.w*3.5/4+c.x0, c.h/4+c.y0);
+        }
+      }
+    };
 
-		var findCell = function(x, y) {
-			var c;
-			for (var i = 0; i < I; ++i) {
-				c = cells[i];
-				if (c.x0 < x && c.x1 > x && c.y0 < y && c.y1 > y) {
-					return c;
-				}
-			}
-		};
+    var findCell = function(x, y) {
+      var c;
+      for (var i = 0; i < I; ++i) {
+        c = cells[i];
+        if (c.x0 < x && c.x1 > x && c.y0 < y && c.y1 > y) {
+          return c;
+        }
+      }
+    };
 
-		var prepareCells = function() {
-			var X, Y = kbProfile.codes.length;
-			var x, y, w, h, rowLayout, rowLabels, rowCodes;
-			var x0 = 0, y0 = 0;
-			var p = 2, P = p*2;
-			h = H / Y;
+    var prepareCells = function() {
+      cells = [];
+      var X, Y = kbProfile.codes.length;
+      var x, y, w, h, rowLayout, rowLabels, rowCodes;
+      var x0 = 0, y0 = 0;
+      var p = 2, P = p*2;
+      h = H / Y;
 
-			for (y = 0; y < Y; ++y) {
-				rowLayout = kbProfile.factorWidths[y];
-				rowLabels = kbProfile.labels[y];
-				rowCodes  = kbProfile.codes[y];
-				X = rowLayout.reduce(sum, 0);
-				for (x = 0; x < rowLayout.length; ++x) {
-					w = W * (rowLayout[x] / X);
-					cells.push({
-						x0: r(x0+p), x1: r(x0+w-P),
-						y0: r(y0+p), y1: r(y0+h-P),
-						w:  r(w-P), h:r(h-P),
-						l:  rowLabels[x].split('\n'),
-						c:  rowCodes[x]
-					});
-					x0 += w;
-				}
-				x0 = 0;
-				y0 += h;
-			}
-			
-			ctx.font = r(h/3) + 'px ' + kbStyle.fontFamily;
-			
-			I = cells.length;
-			
-			for (var i = 0; i < I; ++i) {
-				draw(cells[i]);
-			}
+      for (y = 0; y < Y; ++y) {
+        rowLayout = kbProfile.factorWidths[y];
+        rowLabels = kbProfile.labels[y];
+        rowCodes  = kbProfile.codes[y];
+        X = rowLayout.reduce(sum, 0);
+        for (x = 0; x < rowLayout.length; ++x) {
+          w = W * (rowLayout[x] / X);
+          cells.push({
+            x0: r(x0+p), x1: r(x0+w-P),
+            y0: r(y0+p), y1: r(y0+h-P),
+            w:  r(w-P), h:r(h-P),
+            l:  rowLabels[x].split('\n'),
+            c:  rowCodes[x]
+          });
+          x0 += w;
+        }
+        x0 = 0;
+        y0 += h;
+      }
+      
+      ctx.font = r(h/3) + 'px ' + kbStyle.fontFamily;
+      
+      I = cells.length;
+      
+      for (var i = 0; i < I; ++i) {
+        draw(cells[i]);
+      }
 
-			ctx.font = r(h/3) + 'px ' + kbStyle.fontFamily;
-			
-			I = cells.length;
-			
-			for (var i = 0; i < I; ++i) {
-				draw(cells[i]);
-			}
-		};
+      ctx.font = r(h/3) + 'px ' + kbStyle.fontFamily;
+      
+      I = cells.length;
+      
+      for (var i = 0; i < I; ++i) {
+        draw(cells[i]);
+      }
+    };
 
 
-		var downCell;
 
-		var onDown = function(ev) {
-			ev.preventDefault();
-			ev.stopPropagation();
-			if (ev.changedTouches) {
-				ev = ev.changedTouches[0];
-			}
-			var c = findCell(ev.clientX, ev.clientY);
-			if (!c) { return; }
-			draw(c, true);
-			downCell = c;
-			//console.log('< %s', c.c);
-			var symb = keyMap[c.c];
-			socket.emit('key', [symb, 1, c.c, c.l]);
-		};
+    init();
+    prepareCells();
 
-		var onUp = function() {
-			var c = downCell;
-			if (!c) { return; }
-			draw(c);
-			//console.log('> %s', c.c);
-			var symb = keyMap[c.c];
-			socket.emit('key', [symb, 0, c.c, c.l]);
-			downCell = undefined;
-		};
 
-		window.addEventListener('mousedown',  onDown);
-		window.addEventListener('mouseup',    onUp);
-		window.addEventListener('touchstart', onDown);
-		window.addEventListener('touchend',   onUp);
 
-		window.addEventListener('load', function() {
-			setTimeout(function(){
-				window.scrollTo(0, 1);
-			}, 0);
-		});
 
-		window.addEventListener('resize', function() {
-			prepareCells();
-		});
+    var downCell;
 
-		prepareCells();
-	};
+    var onDown = function(ev) {
+      ev.preventDefault();
+      ev.stopPropagation();
+      if (ev.changedTouches) {
+        ev = ev.changedTouches[0];
+      }
+      var c = findCell(ev.clientX, ev.clientY);
+      if (!c) { return; }
+      draw(c, true);
+      downCell = c;
+      //console.log('< %s', c.c);
+      var symb = keyMap[c.c];
+      socket.emit('key', [symb, 1, c.c, c.l]);
+    };
 
-	window.carrie = carrie;
+    var onUp = function() {
+      var c = downCell;
+      if (!c) { return; }
+      draw(c);
+      //console.log('> %s', c.c);
+      var symb = keyMap[c.c];
+      socket.emit('key', [symb, 0, c.c, c.l]);
+      downCell = undefined;
+    };
+
+    window.addEventListener('mousedown',  onDown);
+    window.addEventListener('mouseup',    onUp);
+    window.addEventListener('touchstart', onDown);
+    window.addEventListener('touchend',   onUp);
+
+    window.addEventListener('load', function() {
+      setTimeout(function(){
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+
+    window.addEventListener('resize', function() {
+      reinit();
+      prepareCells();
+    });
+  };
+
+  window.carrie = carrie;
 
 })();
--- web/keyboard-win-pt.js
+++ web/keyboard-win-pt.js
@@ -1,44 +1,44 @@
 
 /*
 at this level, each key has:
-	- c code it send as its identify when used
-	- l label to display in the canvas
-	- fw factor of width usage: relative size of the key relating to its neighbors
+  - c code it send as its identify when used
+  - l label to display in the canvas
+  - fw factor of width usage: relative size of the key relating to its neighbors
 */
 
 var fws = [
-	[1,    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 13
+  [1,    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 13
 
-	[1,    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.4], // 14
-	[1.5,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.25], // 14
-	[1.8,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.1], // 14
-	[1.5,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.3], // 13
-	[1, 1, 7, 1, 1] // 5
+  [1,    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.4], // 14
+  [1.5,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.25], // 14
+  [1.8,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.1], // 14
+  [1.5,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.3], // 13
+  [1, 1, 7, 1, 1] // 5
 ];
 
 var ls = [
-	['esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'], //13
+  ['esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'], //13
 
-	['\\\n|', '1\n!', '2\n"\n@', '3\n#\n£', '4\n$', '5\n%', '6\n&', '7\n/\n{', '8\n(\n[', '9\n)\n]', '0\n=\n}', '\'\n?', '«\n»', 'bksp'], // 14
-	['→', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '+\n*', '´\n`', '↵'], // 14
-	['caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç', 'º\nª', '~\n^', ' '], // 14
-	['⇑', '<\n>', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',\n;', '.\n:', '-\n_', '⇑'], // 13
-	['ctr', 'alt', ' ', 'alt gr', 'ctr'] // 5
+  ['\\\n|', '1\n!', '2\n"\n@', '3\n#\n£', '4\n$', '5\n%', '6\n&', '7\n/\n{', '8\n(\n[', '9\n)\n]', '0\n=\n}', '\'\n?', '«\n»', 'bksp'], // 14
+  ['→', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '+\n*', '´\n`', '↵'], // 14
+  ['caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç', 'º\nª', '~\n^', ' '], // 14
+  ['⇑', '<\n>', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',\n;', '.\n:', '-\n_', '⇑'], // 13
+  ['ctr', 'alt', ' ', 'alt gr', 'ctr'] // 5
 ];
 
 var cs = [
-	['ESC', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'], // 13
+  ['ESC', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'], // 13
 
-	['BACKSLASH', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'MINUS', '', 'BACKSPACE'], // 14
-	['TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'EQUAL', 'RIGHTBRACE', 'ENTER'], // 14
-	['CAPSLOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'SEMICOLON', 'LEFTBRACE', 'APOSTROPHE', 'ENTER'], // 14
-	['LEFTSHIFT', '102ND', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'COMMA', 'DOT', 'SLASH', 'RIGHTSHIFT'], // 13
-	['LEFTCTRL', 'LEFTALT', 'SPACE', 'RIGHTALT', 'RIGHTCTRL'] // 5
+  ['BACKSLASH', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'MINUS', '', 'BACKSPACE'], // 14
+  ['TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'EQUAL', 'RIGHTBRACE', 'ENTER'], // 14
+  ['CAPSLOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'SEMICOLON', 'LEFTBRACE', 'APOSTROPHE', 'ENTER'], // 14
+  ['LEFTSHIFT', '102ND', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'COMMA', 'DOT', 'SLASH', 'RIGHTSHIFT'], // 13
+  ['LEFTCTRL', 'LEFTALT', 'SPACE', 'RIGHTALT', 'RIGHTCTRL'] // 5
 ];
 
 
 var carrieKeyboardProfile = {
-	codes:        cs,
-	labels:       ls,
-	factorWidths: fws
+  codes:        cs,
+  labels:       ls,
+  factorWidths: fws
 };
